6d08631cee2cf265cc7ab0ca8c9446df32a442b5
jcahal <jacahal@gmail.com>
2026-03-30 18:40:51 -0700

    v2026 - updated for use on render migrating away from gh pages/heroku/mongodb atlas

--- server/api/server.js
+++ server/api/server.js
@@ -1,69 +1,46 @@
 /**
  * GNITO API
- * 
+ *
  * Author: jcahal
- * Version: 0.0.3
- * 
- * TODO: 
- *  Fix delete after retrieve
- *  Comment tf out of this file
- * 
+ * Version: 0.0.5
  */
 
-const express = require('express');
-var bodyParser = require('body-parser')
-const mongoose = require('mongoose');
-const cors = require('cors');
-const { version } = require('os');
-const { json } = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-// create application/json parser
 const jsonParser = bodyParser.json()
- 
-// create application/x-www-form-urlencoded parser
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
-// DB Connection
-const DBA = process.env.DB_ADDRESS || 'cluster0.faqgv.mongodb.net';
-const PR = process.env.DB_PROTOCOL || 'mongodb+srv';
-const DB = process.env.DB_NAME || 'gnito';
-const UN = process.env.DB_USERNAME || 'gnito-client';
-const PW = process.env.DB_PASSWORD || 'E3oKn3I8CInd';
-mongoose.connect(`${PR}://${UN}:${PW}@${DBA}/${DB}`, {
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/gnito'
+mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
-});
+  useUnifiedTopology: true,
+})
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
-  console.log(`Connected to ${PR}://${UN}:${PW}@${DBA}/${DB}`)
+  console.log(`Connected to ${mongoUrl}`)
 })
 
-const Schema = mongoose.Schema
-const Model = mongoose.model
-
-const dropSchema = new mongoose.Schema({
-  message: String,
+const Drop = mongoose.model('Drop', new mongoose.Schema({
+  message:  String,
   password: String,
-  title: String,
-})
+  title:    String,
+}))
 
-const Drop = Model('Drop', dropSchema)
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port)
 
 app.get('/', (req, res) => {
   res.send('Hello from the Server!')
@@ -71,21 +48,19 @@ app.get('/', (req, res) => {
 
 app.route('/api/')
   .get((req, res) => {
-    res.json({
-      name: 'gnito-api',
-      version: '0.0.3'
-    })
+    res.json({ name: 'gnito-api', version: '0.0.5' })
   })
   .post(jsonParser, (req, res) => {
     Drop.create(req.body, (err, drop) => {
+      if (err) return res.status(500).json({ error: 'Failed to create drop' })
       res.json(drop)
     })
   })
 
-// TODO: figure out how to get with special characters in password
 app.route('/api/:drop')
-  .get( (req, res) => {
-    Drop.findOneAndRemove({ _id: req.params.drop, password: `${req.query.pwd}` }, (err, drop) => {
+  .get((req, res) => {
+    Drop.findOneAndRemove({ _id: req.params.drop, password: req.query.pwd }, (err, drop) => {
+      if (err || !drop) return res.status(404).json({ error: 'Drop not found or incorrect password' })
       res.json(drop)
     })
   })
